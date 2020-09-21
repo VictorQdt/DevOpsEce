ced638991fab08d8ccb8c332132f2728502be20e
Victor Quidet <victor.Quidet@gmail.com>
2020-09-21 16:53:24 +0200

    Début TP1

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
+const app = express();
 
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
   res.send('Hello World!')
-})
+});
 
-module.exports = app.listen(port, (err) => {
-  if (err) throw err
-  console.log("Server listening the port " + port)
-})
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
