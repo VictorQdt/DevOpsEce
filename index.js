e7e5ebc05bdf7eb89c543567614c5455f71ad1f8
Victor Quidet <victor.Quidet@gmail.com>
2020-09-21 22:25:09 +0200

    Mise en place routes index

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+let response = { "page": "home" };
+res.send(JSON.stringify(response));
+});
+
+app.get('/users/:id(\\d+)', function(req, res) {
+let response = { "page": "users", "parameters": req.params };
+res.send(JSON.stringify(response));
+});
+app.post('/users', function(req, res) {
+let response = { "page": "users", "parameters": req.params };
+res.send(JSON.stringify(response));
+});
+app.put('/users/:id(\\d+)', function(req, res) {
+let response = { "page": "users", "parameters": req.params };
+res.send(JSON.stringify(response));
+});
+app.delete('/users/:id(\\d+)', function(req, res) {
+let response = { "page": "users", "parameters": req.params };
+res.send(JSON.stringify(response));
+});
+
+app.get('/users/messages', function(req, res) {
+let response = { "page": "messages" };
+res.send(JSON.stringify(response));
 });
 
 app.listen(3000, function() {
